c372140a3afd8659f6f08032db3702fd80f2befe
209410686 <209410686@gms.tku.edu.tw>
2021-10-13 20:18:29 +0800

    add

--- utils/database.js
+++ utils/database.js
@@ -1,16 +1,19 @@
 const {Pool} = require('pg')
 
+const isProduction = process.env.MoDE_ENV ==="production"
+
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'crown_86',
-  password: '0000',
-  port: 5432,
+  //user: 'postgres',
+  //host: 'localhost',
+  //database: 'crown_86',
+  //password: '0000',
+  //port: 5432,
+  connectionString: isProduction? process.env.DATABASE : `postgresql://postgres:0000@localhost:5432/crown86`
 })
-pool.query('SELECT * from category', (err, res) => {
-  console.log(JSON.stringify(res.rows));
-  pool.end();
-});
+//pool.query('SELECT * from category', (err, res) => {
+ // console.log(JSON.stringify(res.rows));
+ //pool.end();
+//});
 
 module.exports = pool;
 
